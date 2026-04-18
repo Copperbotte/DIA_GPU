d7bf3fe02d2b41971fec6999a30294cc03b1660d
Joe Kessler <copperbotte@gmail.com>
2026-04-17 20:54:30 -0700

    Update clean.py from Python 2 to Python 3

--- clean.py
+++ clean.py
@@ -15,6 +15,8 @@ from astropy.nddata.utils import Cutout2D
 from astropy.wcs import WCS
 import math
 import time
+import argparse
+import sys
 
 #libraries for image registration
 import FITS_tools
@@ -35,10 +37,24 @@ flatdiv = 0 # yes = 1 no = 0 to flat field
 align = 1# yes = 1 no = 0 to align based on coordinates
 
 #useful directories
-rawdir = '.../cal/' #directory with the raw images
-cdedir = '.../code/clean/' #directory where the code 'lives'
+#rawdir = '.../cal/' #directory with the raw images
+#cdedir = '.../code/clean/' #directory where the code 'lives'
 caldir = 'N/A' #directory with the calibration images such as bias & flat
-clndir = '../clean/'#directory for the cleaned images to be output
+#clndir = '../clean/'#directory for the cleaned images to be output
+
+# Use forward slashes so the path works on Windows without escaping
+from pathlib import Path
+ROOT = Path('C:/Users/Joe/Desktop/Projects/2026_Spring/DIA/')
+cdedir = ROOT / "DIA" / "routines" / "Python"
+#rawdir = ROOT / "DIA_TEMP" / "raw"
+rawdir = ROOT / "TESS_sector_4"
+clndir = ROOT / "DIA_TEMP" / "clean"
+
+# ensure the output directories exist
+ROOT = Path(ROOT)#.mkdir(parents=True, exist_ok=True)
+cdedir = Path(cdedir)#.mkdir(parents=True, exist_ok=True)
+rawdir = Path(rawdir)#.mkdir(parents=True, exist_ok=True)
+clndir = Path(clndir)#.mkdir(parents=True, exist_ok=True)
 
 #sample every how many pixels? usually 32x32 is OK but it can be larger or smaller
 pix = 32 # UPDATE HERE FOR BACKGROUND SPACING
@@ -46,15 +62,38 @@ axs = 2048 # UPDATE HERE FOR IMAGE AXIS SIZE
 ###END UPDATE INFORMATION###
 
 #get the image list and the number of files which need reduction
-os.chdir(rawdir) #changes to the raw image direcotory
-files = [f for f in glob.glob("*.fits") if isfile(join(rawdir, f))] #gets the relevant files with the proper extension
+#os.chdir(rawdir) #changes to the raw image direcotory
+files = [f for f in rawdir.glob("*.fits") if isfile(join(rawdir, f))] #gets the relevant files with the proper extension
+
+# Cull files that don't match the specified camera and CCD
+camera, ccd = '1', '4'
+def get_camera_and_ccd(f):
+	# with fits.open(f, memmap=True) as hdul:
+	# 	camera = hdul[1].header['CAMERA']
+	# 	ccd = hdul[1].header['CCD']
+	# Grab these from the filename instead due to perf loss
+	# tess2018297215939-s0004-1-4-0124-s_ffic.fits
+	filename = f.stem  # gets filename without extension
+	parts = filename.split('-')
+	camera = parts[2]
+	ccd = parts[3]
+	return camera, ccd
+
+def filter_file(f):
+	#img_data, img_header = fits.getdata(f, header=True)
+	#img_header = fits.getheader(f)
+	# Get these from the data's header manually
+	camera_val, ccd_val = get_camera_and_ccd(f)
+	return (camera_val == camera) and (ccd_val == ccd)
+files = list(filter(filter_file, files))
+
 files.sort()
 nfiles = len(files)
-os.chdir(cdedir) #changes back to the code directory
+#os.chdir(cdedir) #changes back to the code directory
 
 #get the zeroth image for registration
 #read in the image
-ref, rhead = fits.getdata(rawdir+files[0], header = True)
+ref, rhead = fits.getdata(os.path.join(rawdir, files[0]), header = True)
 rhead['CRPIX1'] = 1001.
 rhead['NAXIS1'] = 2048
 rhead['NAXIS2'] = 2048
@@ -62,43 +101,50 @@ rhead['NAXIS2'] = 2048
 #sample every how many pixels?
 bxs = 512 #how big do you want to make the boxes for each image?
 lop = 2*pix
-sze = (bxs/pix)*(bxs/pix)+2*(bxs/pix)+1 #size holder for later
+sze = int((bxs//pix)*(bxs//pix) + 2*(bxs//pix) + 1) #size holder for later
 
 #read in the flat
 if (flatdiv == 1):
-	flist = fits.open(caldir+'flat.fits')
+	flist = fits.open(os.path.join(caldir, 'flat.fits'))
 	fheader = flist[0].header #get the header info
 	flat = flist[0].data #get the image info
 
 #read in the bias
 if (biassub == 1):
-	blist = fits.open(caldir+'bias.fits')
+	blist = fits.open(os.path.join(caldir, 'bias.fits'))
 	bheader = blist[0].header #get the header info
 	bias = blist[0].data #get the image info
 
+global_t0 = time.time()
 #begin cleaning
 for ii in range(0, nfiles):
-	hld = files[ii].split('.')
+	if ii == 1:
+		break #DEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUGDEBUG
+
+	file_stem = files[ii].stem  # gets filename without extension
 
 	#update the name to be appropriate for what was done to the file
 	if (biassub == 0) and (flatdiv == 0) and (align == 0): 
-		finnme = hld[0]+'_s.fits'
+		finnme = f'{file_stem}_s.fits'
 	if (biassub == 1) and (flatdiv == 1) and (align == 0):
-		finnme = hld[0]+'_sfb.fits'
+		finnme = f'{file_stem}_sfb.fits'
 	if (biassub == 0) and (flatdiv == 0) and (align == 1):
-		finnme = hld[0]+'_sa.fits'
+		finnme = f'{file_stem}_sa.fits'
 	if (biassub == 1) and (flatdiv == 1) and (align == 1):
-		finnme = hld[0]+'_sfba.fits'
+		finnme = f'{file_stem}_sfba.fits'
+
+	outpath = clndir / finnme
 
 	#only create the files that don't exist
-	if (os.path.isfile(clndir+finnme) == 0):
-    		#start the watch
-    		st = time.time()
+	#if not outpath.exists():
+	if True:
+		#start the watch
+		st = time.time()
 		sts = time.strftime("%c")
-		print 'Now cleaning '+files[ii]+' at '+sts+'.'
+		print(f'Now cleaning {files[ii]} at {sts}.')
 
 		#read in the image
-		orgimg, header = fits.getdata(rawdir+files[ii], header = True)
+		orgimg, header = fits.getdata(files[ii], header = True)
 		w = WCS(header)
 		cut = Cutout2D(orgimg, (1068,1024), (axs, axs), wcs = w)
 		bigimg = cut.data
@@ -109,9 +155,10 @@ for ii in range(0, nfiles):
 		header['NAXIS2'] = 2048
 
 		#get the holders ready
-		res = numpy.zeros(shape=(axs, axs)) #holder for the background 'image'
-		bck = numpy.zeros(shape=((axs/bxs)**2)) #get the holder for the image backgroudn
-		sbk = numpy.zeros(shape=((axs/bxs)**2)) #get the holder for the sigma of the image background
+		res = numpy.zeros(shape=(axs, axs), dtype=float) #holder for the background 'image'
+		nboxes = int((axs//bxs)**2)
+		bck = numpy.zeros(shape=(nboxes,), dtype=float) #get the holder for the image backgroudn
+		sbk = numpy.zeros(shape=(nboxes,), dtype=float) #get the holder for the sigma of the image background
 
 		#remove the flat and the bias
 		if (biassub == 1) and (flatdiv == 1):
@@ -121,21 +168,24 @@ for ii in range(0, nfiles):
 		for oo in range(0, axs, bxs):
 			for ee in range(0, axs, bxs):
 				img = bigimg[ee:ee+bxs, oo:oo+bxs] #split the image into small subsections
+				print(f"{img.dtype = }")
 				
 				#calculate the sky statistics
 				cimg, clow, chigh = scipy.stats.sigmaclip(img, low=2.5, high = 2.5) #do a 2.5 sigma clipping
 				sky = numpy.median(cimg) #determine the sky value
 				sig = numpy.std(cimg) #determine the sigma(sky)
+				print(f"{type(sky) = }, {type(sig) = }")
 
 				bck[tts] = sky #insert the image median background
 				sbk[tts] = sig #insert the image sigma background
+				print(f"{bck[tts].dtype = }, {sbk[tts].dtype = }")
 
 				#create holder arrays for good and bad pixels
-				x = numpy.zeros(shape=(sze))
-				y = numpy.zeros(shape=(sze))
-				v = numpy.zeros(shape=(sze))
-				s = numpy.zeros(shape=(sze))
-				nd = long(0)
+				x = numpy.zeros(shape=(int(sze),), dtype=float)
+				y = numpy.zeros(shape=(int(sze),), dtype=float)
+				v = numpy.zeros(shape=(int(sze),), dtype=float)
+				s = numpy.zeros(shape=(int(sze),), dtype=float)
+				nd = 0
 	
 				#begin the sampling of the "local" sky value
 				for jj in range(0, bxs+pix, pix):
@@ -168,8 +218,9 @@ for ii in range(0, nfiles):
 
 					for jj in range(0, len(rj[0])):
 						#select the bad point
-						xbad = x[rj[jj]]
-						ybad = y[rj[jj]]
+						idx = rj[0][jj]
+						xbad = x[idx]
+						ybad = y[idx]
 						#use the distance formula to get the closest points
 						rd = math.sqrt((xgood-ygood)**2.+(ygood-ybad)**2.)
 						#sort the radii
@@ -178,7 +229,7 @@ for ii in range(0, nfiles):
 						vnear = vgood[pp[0:9]]
 						ave = numpy.median(vnear)
 						#insert the good value into the array
-						v[rj[jj]] = ave
+						v[idx] = ave
 
 				#now we want to remove any possible values which have bad sigmas
 				rjs = numpy.where(s >= 2*sig)
@@ -195,8 +246,9 @@ for ii in range(0, nfiles):
 
 					for jj in range(0, len(rj)):
 						#select the bad point
-						xbad = x[rj[jj]]
-						ybad = y[rj[jj]]
+						idx = int(rj[jj])
+						xbad = x[idx]
+						ybad = y[idx]
 						#print xbad, ybad
 						#use the distance formula to get the closest points
 						rd = numpy.sqrt((xgood-xbad)**2.+(ygood-ybad)**2.)
@@ -206,7 +258,7 @@ for ii in range(0, nfiles):
 						vnear = vgood[pp[0:9]]
 						ave = numpy.median(vnear)
 						#insert the good value into the array
-						v[rj[jj]] = ave
+						v[idx] = ave
 
 				#now we interpolate to the rest of the image with a thin-plate spline	
 				xi = numpy.linspace(0, bxs-1, bxs)
@@ -214,9 +266,11 @@ for ii in range(0, nfiles):
 				XI, YI = numpy.meshgrid(xi, yi)
 				rbf = Rbf(x, y, v, function = 'thin-plate', smooth = 0.0)
 				reshld = rbf(XI, YI)
+				print(f"{reshld.dtype = }")
 			
 				#now add the values to the residual image
 				res[ee:ee+bxs, oo:oo+bxs] = reshld
+				print(f"{res.dtype = }")
 				tts = tts+1
 
 		#get the median background
@@ -226,9 +280,13 @@ for ii in range(0, nfiles):
 		#subtract the sky gradient and add back the median background
 		sub = bigimg-res
 		sub = sub + mbck
+		print(f"{sub.dtype = }", f"{bigimg.dtype = }", f"{res.dtype = }", f"{mbck.dtype = }")
 
 		#align the image
+		# NOTE: `hcongrid` was originally used for alignment. If available,
+		# uncomment the import at the top and ensure the function is on PATH.
 		algn = hcongrid(sub, header, rhead)
+		print(f"{algn.dtype = }")
 
 		#update the header
 		header['CTYPE1'] = rhead['CTYPE1']
@@ -255,10 +313,12 @@ for ii in range(0, nfiles):
 
 		#write out the subtraction
 		shd = fits.PrimaryHDU(algn, header=header)
-		shd.writeto(clndir+finnme, overwrite = True)
-    	
-		#stop the watch
-    		fn = time.time()
-    		print 'Background subtraction for '+files[ii]+' finished in '+str(fn-st)+'s.'
+		shd.writeto(outpath, overwrite=True)
 
-print 'All done! See ya later alliagtor.'
+		#stop the watch
+		fn = time.time()
+		print(f'Background subtraction for {files[ii]} finished in {fn-st}s.')
+
+print('All done! See ya later alliagtor.')
+global_t1 = time.time()
+print(f'Total processing time: {global_t1 - global_t0}s')
